3fde95b9b6b409d744372373a5e1b2eb945a7d39
sjitesh976-sys <sjitesh976@gmail.com>
2025-11-25 23:58:54 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,12 +1,21 @@
-{
-  "name": "webrtc-signal-server",
-  "version": "1.0.0",
-  "main": "server.js",
-  "scripts": {
-    "start": "node server.js"
-  },
-  "dependencies": {
-    "express": "^4.18.2",
-    "ws": "^8.13.0"
-  }
+import express from "express";
+import { WebSocketServer } from "ws";
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+// HTTP server
+const server = app.listen(port, () => {
+  console.log("Server running on port " + port);
+});
+
+// WebSocket server
+const wss = new WebSocketServer({ server });
+
+wss.on("connection", (socket) => {
+  console.log("User connected");
+
+  socket.on("message", (msg) => {
+    socket.send(msg);
+  });
 }
