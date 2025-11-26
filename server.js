872069095efc66579c73fd654c42bb3095507200
sjitesh976-sys <sjitesh976@gmail.com>
2025-11-26 14:18:10 +0530

    server.js

--- server.js
+++ server.js
@@ -1,20 +1,15 @@
-import express from "express";
-import path from "path";
-import { fileURLToPath } from "url";
+const express = require("express");
+const path = require("path");
 
 const app = express();
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
+const PORT = process.env.PORT || 3000;
 
-// Static public folder
 app.use(express.static(path.join(__dirname, "public")));
 
-// Send main page
-app.get("/", (req, res) => {
+app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname, "public", "index.html"));
 });
 
-const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
