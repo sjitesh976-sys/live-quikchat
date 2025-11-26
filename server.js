18c2da1e7ac644385c5eb6ab9144920db66fc163
sjitesh976-sys <sjitesh976@gmail.com>
2025-11-26 16:38:17 +0530

    server.js

--- server.js
+++ server.js
@@ -19,31 +19,44 @@ let waitingUser = null;
 io.on("connection", (socket) => {
   console.log("User connected:", socket.id);
 
+  // Find Partner
   socket.on("findPartner", () => {
-    if (waitingUser) {
+    if (waitingUser && waitingUser !== socket.id) {
       io.to(socket.id).emit("partnerFound", waitingUser);
       io.to(waitingUser).emit("partnerFound", socket.id);
       waitingUser = null;
     } else {
       waitingUser = socket.id;
+      console.log("Waiting user:", waitingUser);
     }
   });
 
+  // Offer SDP
   socket.on("offer", (data) => {
-    io.to(data.partner).emit("offer", data.sdp);
+    io.to(data.partner).emit("offer", {
+      sdp: data.sdp,
+      from: socket.id
+    });
   });
 
+  // Answer SDP
   socket.on("answer", (data) => {
-    io.to(data.partner).emit("answer", data.sdp);
+    io.to(data.partner).emit("answer", {
+      sdp: data.sdp,
+      from: socket.id
+    });
   });
 
+  // ICE Candidate
   socket.on("iceCandidate", (data) => {
     io.to(data.partner).emit("iceCandidate", data.candidate);
   });
 
+  // Disconnect
   socket.on("disconnect", () => {
     if (waitingUser === socket.id) waitingUser = null;
+    socket.broadcast.emit("partnerDisconnected");
   });
 });
 
-server.listen(3000, () => console.log("Server running on port 3000"));
+server.listen(3000, () => console.log("🚀 Server running on port 3000"));
