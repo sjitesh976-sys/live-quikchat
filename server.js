11837a69c81fe5cb5dab0d5caecc1a42bfb5dad5
sjitesh976-sys <sjitesh976@gmail.com>
2025-11-26 16:08:51 +0530

    server.js

--- server.js
+++ server.js
@@ -1,17 +1,49 @@
-const express = require("express");
-const path = require("path");
+import express from "express";
+import http from "http";
+import { Server } from "socket.io";
+import cors from "cors";
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+app.use(cors());
+app.use(express.static("public"));
 
-// serve static files inside public folder
-app.use(express.static(path.join(__dirname, "public")));
-
-// serve index.html for all routes
-app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "public", "index.html"));
+const server = http.createServer(app);
+const io = new Server(server, {
+  cors: {
+    origin: "*",
+  }
 });
 
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+let waitingUser = null;
+
+io.on("connection", (socket) => {
+  console.log("User connected:", socket.id);
+
+  socket.on("findPartner", () => {
+    if (waitingUser) {
+      io.to(socket.id).emit("partnerFound", waitingUser);
+      io.to(waitingUser).emit("partnerFound", socket.id);
+      waitingUser = null;
+    } else {
+      waitingUser = socket.id;
+    }
+  });
+
+  socket.on("offer", (data) => {
+    io.to(data.partner).emit("offer", data.sdp);
+  });
+
+  socket.on("answer", (data) => {
+    io.to(data.partner).emit("answer", data.sdp);
+  });
+
+  socket.on("iceCandidate", (data) => {
+    io.to(data.partner).emit("iceCandidate", data.candidate);
+  });
+
+  socket.on("disconnect", () => {
+    if (waitingUser === socket.id) waitingUser = null;
+  });
 });
+
+server.listen(3000, () => console.log("Server running on port 3000"));
